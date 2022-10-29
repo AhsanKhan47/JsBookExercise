249c8c45181ec658b1dc6a7662e543eb2007c9a9
Ahsan Khan <ahsankhanafridi2000@gmail.com>
2022-10-29 11:04:50 -0700

    first two projects completed for chapter 4

--- chap_4/projects.mjs
+++ chap_4/projects.mjs
@@ -1,9 +1,41 @@
 import PromptSync from "prompt-sync";
-let prompt = PromptSync;
+let prompt = PromptSync();
 
-let randomNumber = Math.random();
-randomNumber = randomNumber * 5;
-randomNumber = Math.floor(randomNumber);
+// Evaluating a number game
 
-let userValue = Number(prompt("Enter the number "));
-let defulte = " less, geater"
+// let randomNumber = Math.random();
+// randomNumber = randomNumber * 5;
+// randomNumber = Math.floor(randomNumber);
+
+// let userValue = Number(prompt("Enter the number "));
+
+// if(userValue === randomNumber){
+//     console.log(`The Number you entered is equal to the dynamic number${randomNumber}`);
+// }
+// else if(userValue > randomNumber){
+//     console.log(`The Number you entered is Greater than the dynamic number ${randomNumber}`);
+// }
+// else if(userValue < randomNumber){
+//     console.log(`The Number you entered is Smaller than the dynamic number${randomNumber}`);
+// }
+
+// Friend checker game
+
+// let aotCharacters = ["Eren", "Makima", "Mikasa", "Armin", "Reiner", "Annie"]
+
+let userInput = prompt("Enter the Aot characters names");
+
+switch(userInput){
+    case "Eren":
+    case "Makima":
+    case "Mikasa":
+    case "Armin":
+    case "Reiner":
+    case "Annie":
+    console.log("Correct! they are aot characters!!! Tatakae");
+    break;
+
+    default:
+    console.log("Ops! they are not Aot characters.")
+    
+}
